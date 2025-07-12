503f2a2f482ce40013a3985ab105a4109bd8be70
nanasarpp <nanasarpp@gmail.com>
2025-07-12 11:07:39 +0000

    This is a budget tracker

--- budget_tracker/budget_tracker.py
+++ budget_tracker/budget_tracker.py
@@ -1,1 +1,23 @@
-Print("Hello name")
+#print welcome message for user
+user_name = input("Input your name: ")
+print(f"Hello {user_name} welcome to budget manager")
+
+#Take user income
+salary = float(input("What is your monthly salary: "))
+bussiness = float(input("What is the amount earn from bussiness: "))
+#Take user expenses
+transport = float(input("How much do you spend on transport: "))
+food = float(input("How much do you spend on food: "))
+
+#calculate user's toatal income
+total_income = salary + bussiness 
+print(f"Total monthly income: $ {total_income:.2f}")
+
+#Calculate user's total expense ed from your bussiness: "))
+
+total_expense = transport + food
+print(f"Total monthly expense: ${total_expense:.2f}")
+
+#savings 
+monthly_savings = total_income - total_expense
+print(f"Hello {user_name} your monthly savings is {monthly_savings: .2f}")
